474a57a018e7473f89fa05a5a5ac50f2f7ae40e2
Pasindu Sandamal Senanayake <138322414+PasinduSandamalSenanayake@users.noreply.github.com>
2024-12-11 22:48:46 +0530

    up to contact done

--- js/script.js
+++ js/script.js
@@ -15,3 +15,53 @@ resumeBtns.forEach((btn, idx) => {
     resumeDetails[idx].classList.add("active");
   });
 });
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .image-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  if (!imgSlide) {
+    console.error("Element '.portfolio-carousel .img-slide' not found!");
+    return;
+  }
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+  // change the index when the arrow right is clicked
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+};
+
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+  activePortfolio();
+});
